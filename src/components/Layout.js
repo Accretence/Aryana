dfb23ec13bda3d7d9748a6f56eda617c3cdc35ef
Accretence <Accretence@users.noreply.github.com>
2022-09-03 01:27:57 +0430

    Working Export Attempt #3

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,34 @@ import Header from './Header.js'
 import Helmet from './Helmet.js'
 import Wrapper from './Wrapper.js'
 
-import defaultProps from '../../config/props.config.js'
+const defaultProps = {
+  useThemeProvider: null,
+  useAuth: null,
+  config: {
+    theme: {
+      width: '900pt',
+    },
+    meta: {
+      title: 'NEXT-DASHBOARD-ABSTRACTION',
+      image: 'https://i.imgur.com/NitQE9d.jpg',
+      url: 'https://example.com',
+      handle: '@example',
+      keywords: 'geist-ui, nextjs, reactjs',
+    },
+    links: {
+      email: 'mailto:example@example.com',
+      twitter: 'https://twitter.com/example',
+      linkedin: 'https://linkedin.com/in/example',
+      github: 'https://github.com/example',
+    },
+    tabs: [
+      {
+        label: 'CONTACT',
+        value: '/contact',
+      },
+    ],
+  },
+}
 
 const Layout = ({
   config,
